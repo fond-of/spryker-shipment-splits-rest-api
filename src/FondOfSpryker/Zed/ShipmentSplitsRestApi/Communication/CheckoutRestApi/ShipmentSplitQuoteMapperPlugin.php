7c9d13735f4614e5b6e26623da6a4d39f466c7f7
Jozsef Geng <gengjozsef86@gmail.com>
2019-11-21 09:08:56 +0100

    refractoring

---
+++ src/FondOfSpryker/Zed/ShipmentSplitsRestApi/Communication/CheckoutRestApi/ShipmentSplitQuoteMapperPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FondOfSpryker\Zed\ShipmentSplitsRestApi\Communication\Plugin\CheckoutRestApi;
+
+use FondOfSpryker\Zed\OrderSplitsRestApiExtension\Dependency\Plugin\QuoteMapperPluginInterface;
+
+/**
+ * @method \FondOfSpryker\Zed\ShipmentSplitsRestApi\Business\ShipmentSplitsRestApiFacadeInterface getFacade()
+ */
+class ShipmentSplitQuoteMapperPlugin extends AbstractPlugin implements QuoteMapperPluginInterface
+{
+
+}
