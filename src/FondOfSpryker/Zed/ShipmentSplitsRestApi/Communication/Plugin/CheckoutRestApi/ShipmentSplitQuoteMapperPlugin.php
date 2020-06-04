d1ab9b8ea3eab8c46b7c54f3f8cd858b081e85a6
Daniel Rose <daniel-rose@gmx.de>
2020-06-04 10:20:27 +0200

    phpstan & phpcs fixes

--- src/FondOfSpryker/Zed/ShipmentSplitsRestApi/Communication/Plugin/CheckoutRestApi/ShipmentSplitQuoteMapperPlugin.php
+++ src/FondOfSpryker/Zed/ShipmentSplitsRestApi/Communication/Plugin/CheckoutRestApi/ShipmentSplitQuoteMapperPlugin.php
@@ -14,7 +14,7 @@ use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 class ShipmentSplitQuoteMapperPlugin extends AbstractPlugin implements ChildQuoteMapperPluginInterface
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      *
      * @api
      *
